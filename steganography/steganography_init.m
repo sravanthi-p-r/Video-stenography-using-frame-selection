6c10675966f0bb1b699a3384b267110bd8d78782
sravanthi-p-r <sravanthipa020@gmail.com>
2026-05-19 17:44:39 -0500

    Create steganography_init.m

---
+++ steganography/steganography_init.m
@@ -0,0 +1,12 @@
+function [dir_input, dir_output, dir_results] = steganography_init()
+% steganography_init() Sets up Matlab workspace for steganography functions
+
+% Ensure correct directory
+cd(home_directory());
+
+% Return input and output directories
+dir_input = 'input\';
+dir_output = 'output\';
+dir_results = 'results\';
+
+end
