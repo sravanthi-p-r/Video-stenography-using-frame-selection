039b52cf6f9cbc47b7a80ec5d4c7467cdb2bbd53
sravanthi-p-r <sravanthipa020@gmail.com>
2026-05-19 17:32:05 -0500

    Create csvwrite_with_headers.m

---
+++ steganography/helper/csvwrite_with_headers.m
@@ -0,0 +1,9 @@
+function csvwrite_with_headers(filename, data, headers)
+
+fid = fopen(filename, 'w');
+fprintf(fid, [headers, '\n']);
+fclose(fid);
+
+dlmwrite(filename, data, '-append', 'precision', '%.6f', 'delimiter', ',');
+
+end
