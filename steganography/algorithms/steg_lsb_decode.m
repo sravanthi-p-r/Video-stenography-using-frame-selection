984ebab46c394917367d1710fe9a5cac746f28a0
sravanthi-p-r <sravanthipa020@gmail.com>
2026-05-19 17:00:09 -0500

    Add LSB decoding function for steganography
    This function extracts binary data from the least significant bits of an image, allowing for message retrieval.

---
+++ steganography/algorithms/steg_lsb_decode.m
@@ -0,0 +1,11 @@
+function message_bin = steg_lsb_decode(steg)
+% steg_lsb_decode Extracts binary data from least significant bits
+% INPUTS
+%   steg        - Image to extract message from.
+% OUTPUTS
+%   message_bin - Extracted message.
+
+message_bin = mod(steg,2);
+message_bin = reshape(message_bin, [], length(length(message_bin)))';
+
+end
