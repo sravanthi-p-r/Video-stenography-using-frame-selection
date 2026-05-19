e95cb0bccf6ccdeff1067400c59c96bf1f6b084c
sravanthi-p-r <sravanthipa020@gmail.com>
2026-05-19 17:29:32 -0500

    Implement bin2str function for binary to string conversion
    Added bin2str function to convert binary to string.

---
+++ steganography/helper/bin2str.m
@@ -0,0 +1,12 @@
+function s = bin2str(a)
+% bin2str() Converts binary into a string
+% INPUTS
+%    a - Input binary. Length must be multiple of 8
+% OUTPUTS
+%    s - Output string
+
+b = char(a(:)'+'0')';
+b = reshape(b, 8, length(a(:))/8)';
+s = char(bin2dec(b))';
+
+end
